c645ef07d9adf6522b6d61554212648dfb2b1aa5
minhhung <minhhunghuynh1106@gmail.com>
2021-06-16 07:47:32 +0700

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Home from "features/Home";
+import NotFound from "components/NotFound";
+
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Redirect exact from="/" to="/home" />
+
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/sign-in" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
