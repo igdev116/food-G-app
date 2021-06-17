2e1fc0d6998858b4f8bc18202945fae5b2057f88
minhhung <minhhunghuynh1106@gmail.com>
2021-06-17 10:41:25 +0700

    update

--- src/components/SignIn/FormSignIn/index.jsx
+++ src/components/SignIn/FormSignIn/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import FieldSignIn from "../FieldSignIn";
-import PrCheckbox from "utils/cusMatUi";
+import PrRedBtn from "components/CusButtons";
 
 import { Link } from "react-router-dom";
+import { Checkbox } from "@material-ui/core";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
@@ -10,37 +11,39 @@ import "./FormSignIn.scss";
 
 function FormSignIn() {
   return (
-    <div className="form">
-      <h2>Sign in to food G</h2>
-      <div className="form__msg">
-        <span>Don't have an account?</span>
-        <Link className="form__msg-btn" to="/home">
-          Sign up
-        </Link>
+    <form className="form">
+      <FieldSignIn
+        icon={<MailOutlineIcon />}
+        type="text"
+        label="Email address"
+        placeholder="Your email"
+      />
+
+      <FieldSignIn
+        icon={<LockOutlinedIcon />}
+        type="password"
+        label="Password"
+        placeholder="Your password"
+      />
+
+      <div className="form__commit">
+        <Checkbox color="primary" className="form__commit-checkbox" />
+        <span>
+          I agree Platform's{" "}
+          <Link to="#">
+            <strong>Terms of Service</strong>
+          </Link>{" "}
+          and{" "}
+          <Link to="#">
+            <strong>Privacy Policy</strong>
+          </Link>
+        </span>
       </div>
 
-      <form>
-        <FieldSignIn
-          icon={<MailOutlineIcon />}
-          type="text"
-          label="Email address"
-        />
-
-        <FieldSignIn
-          icon={<LockOutlinedIcon />}
-          type="password"
-          label="Password"
-        />
-
-        <div className="form__commit">
-          <PrCheckbox className="form__commit-checkbox" />
-          <span>
-            I agree Platform's <strong>Terms of Service</strong> and{" "}
-            <strong>Privacy Policy</strong>
-          </span>
-        </div>
-      </form>
-    </div>
+      <div className="signin-btn-wrapper">
+        <PrRedBtn>Sign in</PrRedBtn>
+      </div>
+    </form>
   );
 }
 
