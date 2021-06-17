d48e080f9e920238ff868aa029840e4cfb688c51
minhhung <minhhunghuynh1106@gmail.com>
2021-06-17 10:41:44 +0700

    remove something

--- src/utils/cusMatUi.js
+++ src/utils/cusMatUi.js
@@ -1,19 +1,6 @@
-import React from "react";
+import { createMuiTheme } from "@material-ui/core";
 
-import { createMuiTheme, withStyles } from "@material-ui/core";
-import Checkbox from "@material-ui/core/Checkbox";
-
-// primary checkbox
-const PrCheckbox = withStyles({
-  root: {
-    "&$checked": {
-      color: "#cc3333",
-    },
-  },
-  checked: {},
-})((props) => <Checkbox color="default" {...props} />);
-
-export default PrCheckbox;
+import PR_RED_COLOR from "constants/colors";
 
 // overrides container, button
 const Theme = createMuiTheme({
@@ -23,12 +10,17 @@ const Theme = createMuiTheme({
         padding: "0 75px !important",
       },
     },
-    MuiIconButton: {
+    MuiCheckbox: {
       root: {
         padding: "3px !important",
       },
     },
   },
+  palette: {
+    primary: {
+      main: PR_RED_COLOR,
+    },
+  },
 });
 
-export { Theme };
+export default Theme;
