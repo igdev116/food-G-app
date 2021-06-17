8ac73c74f6a71b69cae6257b0f5e6e841bed517b
minhhung <minhhunghuynh1106@gmail.com>
2021-06-17 22:53:15 +0700

    add banner component

---
+++ src/features/Home/components/HomeBanners/HomeBanner/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import PrRedBtn from "components/CusButtons";
+
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+
+import "./HomeBanner.scss";
+
+HomeBanner.propsTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  strong: PropTypes.string,
+  className: PropTypes.string,
+};
+
+HomeBanner.defaultProps = {
+  title: "",
+  description: "",
+  strong: "",
+  className: "",
+};
+
+function HomeBanner(props) {
+  const { title, description, strong, className } = props;
+
+  return (
+    <>
+      <div className={`banner ${className}`}>
+        <div className="banner__container">
+          <div className="banner__title">{title}</div>
+          <div className="banner__description">
+            {description}
+            <strong>{strong}</strong>
+          </div>
+          <PrRedBtn>
+            <AddShoppingCartIcon class="banner__icon" />
+            Order now
+          </PrRedBtn>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeBanner;
