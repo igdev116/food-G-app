b2b5afa52d419995b96be5d53cc96929402cc878
minhhung <minhhunghuynh1106@gmail.com>
2021-06-22 07:38:27 +0700

    add svg data

---
+++ src/features/Home/components/HomeCategory/HomeCategoryDatas.jsx
@@ -0,0 +1,51 @@
+import Hamburger from "../../svgs/hamburger.svg";
+import Coffee from "../../svgs/coffee.svg";
+import Dinner from "../../svgs/dinner.svg";
+import Lunch from "../../svgs/lunch.svg";
+import Tea from "../../svgs/tea.svg";
+import Juice from "../../svgs/juice.svg";
+import Pork from "../../svgs/pork.svg";
+import Chicken from "../../svgs/chicken.svg";
+import Beef from "../../svgs/beef.svg";
+
+const homeCategoryDatas = [
+  {
+    svg: Hamburger,
+    name: "breakfast",
+  },
+  {
+    svg: Coffee,
+    name: "coffee",
+  },
+  {
+    svg: Pork,
+    name: "pork ham",
+  },
+  {
+    svg: Dinner,
+    name: "dinner",
+  },
+  {
+    svg: Tea,
+    name: "tea",
+  },
+  {
+    svg: Lunch,
+    name: "lunch",
+  },
+  {
+    svg: Juice,
+    name: "juice",
+  },
+  {
+    svg: Chicken,
+    name: "grilled chicken",
+  },
+  {
+    svg: Beef,
+    name: "roast beef",
+  },
+];
+
+export { Beef };
+export default homeCategoryDatas;
