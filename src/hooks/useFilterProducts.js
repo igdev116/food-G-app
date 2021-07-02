8c588b2ae221188ee5ca9fbf32975bdc51fc5b0d
minhhung <minhhunghuynh1106@gmail.com>
2021-07-01 22:35:44 -0700

    update logic

--- src/hooks/useFilterProducts.js
+++ src/hooks/useFilterProducts.js
@@ -2,15 +2,24 @@ import shopApi from "api/shopApi";
 import { filterShop } from "features/Shop/shopSlice";
 
 import { useDispatch } from "react-redux";
+import { useLocation, useParams } from "react-router-dom";
+import { usePrevious } from "./usePrevious";
 
 export const useFilterProducts = () => {
   const dispatch = useDispatch();
+  const { name } = useParams();
+  const { pathname } = useLocation();
 
-  return async (type, params) => {
+  const prevPathname = usePrevious(pathname);
+
+  return async (params) => {
     try {
-      const response = await shopApi.getAll(type, params);
-      const action = filterShop(response);
-      dispatch(action);
+      if (pathname !== prevPathname) {
+        console.log(name, params);
+        const response = await shopApi.getAll(name, params);
+        const action = filterShop(response);
+        dispatch(action);
+      }
     } catch (error) {
       console.log(error.message);
     }
