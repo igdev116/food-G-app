3cbbadd3461d85d8ed8e3ae572391753a3aca62d
minhhung <minhhunghuynh1106@gmail.com>
2021-07-01 22:35:09 -0700

    add filter logic

--- src/features/Shop/components/ShopContainer/ShopContent/ShopHandle/index.jsx
+++ src/features/Shop/components/ShopContainer/ShopContent/ShopHandle/index.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+import { useParams } from "react-router-dom";
+
+import { useFilterProducts } from "hooks/useFilterProducts";
 
 import SearchIcon from "@material-ui/icons/Search";
 import ViewList from "@material-ui/icons/ViewList";
@@ -7,7 +11,45 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import "./ShopHandle.scss";
 
+const dataTypes = [
+  {
+    value: "Price: Low to High",
+    sort: { _sort: "price", _order: "dsc" },
+  },
+  {
+    value: "Price: High to Low",
+    sort: "htl",
+  },
+  {
+    value: "Rate: Low to High",
+    sort: "lth",
+  },
+  {
+    value: "Rate: High to Low",
+    sort: "lth",
+  },
+];
+
 function ShopHandle() {
+  const [isDrop, setIsDrop] = useState(false);
+  const ref = useRef();
+
+  const { name } = useParams();
+
+  const filterProducts = useFilterProducts();
+
+  window.addEventListener("click", (e) => {
+    if (ref.current.contains(e.target)) {
+      setIsDrop(!isDrop);
+    } else {
+      setIsDrop(false);
+    }
+  });
+
+  const onFilterProducts = (sort) => {
+    filterProducts(name, sort);
+  };
+
   return (
     <div className="shop-handle">
       <form className="shop-handle__search">
@@ -18,16 +60,25 @@ function ShopHandle() {
       </form>
 
       <div className="shop-handle__drop">
-        <div className="shop-handle__drop-current">
+        <div ref={ref} className="shop-handle__drop-current">
           <span>Featured</span>
           <ExpandMoreIcon />
         </div>
 
-        <ul className="shop-handle__drop-list">
-          <li className="shop-handle__drop-item">Price: Low to High</li>
-          <li className="shop-handle__drop-item">Price: Low to High</li>
-          <li className="shop-handle__drop-item">Price: Low to High</li>
-          <li className="shop-handle__drop-item">Price: Low to High</li>
+        <ul
+          className={
+            isDrop ? "shop-handle__drop-list drop" : "shop-handle__drop-list"
+          }
+        >
+          {dataTypes.map(({ value, sort }, index) => (
+            <li
+              onClick={() => onFilterProducts(sort)}
+              key={index}
+              className="shop-handle__drop-item"
+            >
+              {value}
+            </li>
+          ))}
         </ul>
       </div>
 
